3671cf149fd22f83357d98cac13222c4744489a9
Mpot7 <r-kisa@mail.ru>
2018-10-05 05:07:48 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var mult=5; i=1; zeros=0;
+  do {
+    numi=Math.pow(mult,i)
+    zeros+=Math.floor(number / numi); // слагаемое number / 5^i
+    i++;
+  } while (numi<=number);   // пока 5^i не превысит число number
+  return zeros;
 }
